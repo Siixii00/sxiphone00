b63e24bfeb3c8e9b97487a3cf275eb26c13fe031
Siixii00 <yaninlin@gmail.com>
2026-05-01 00:25:29 +0800

    0.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import { cloudflare } from '@cloudflare/vite-plugin-cloudflare'
 
 export default defineConfig({
-  plugins: [cloudflare()],
   build: {
     outDir: 'dist',
     rollupOptions: {
